2392de4f3a94d10ebb71ef96df0bc9fc4428b3be
slyofzero <sly.of.zero@gmail.com>
2024-06-05 11:58:08 +0530

    Wallet change

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,7 @@ export const splitPaymentsWith: {
   [key: string]: { address: string; share: number };
 } = {
   dev: {
-    address: "0x6AB8715bAE83484006574db2DF9d2186D3AA575d",
+    address: "0xdE2193378CA3fb5A256a8F86a8F4513Fc68aB4DA",
     share: 1,
   },
 };
